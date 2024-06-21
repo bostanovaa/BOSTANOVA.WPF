1c6c9523b43466b0075b22ee6dd680f790e6142d
bostanovaa <168176646+bostanovaa@users.noreply.github.com>
2024-06-21 21:18:51 +0300

    Add files via upload

---
+++ Лабораторная 8 Анимированные трансформации/Лаб 8/Lab8.1/Lab8.1/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab8._1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ScaleTransform scaleTransform = new ScaleTransform();
+        private object textBox1;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Привязка свойства ScaleX и ScaleY преобразования scaleTransform к значению Slider
+            scaleSlider.ValueChanged += OnScaleSliderValueChanged;
+
+            // Начальное применение преобразования scaleTransform к StackPanel с элементами управления
+            rootElement.LayoutTransform = scaleTransform;
+        }
+        private void OnScaleSliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Обновление значений ScaleX и ScaleY преобразования scaleTransform
+            scaleTransform.ScaleX = scaleSlider.Value;
+            scaleTransform.ScaleY = scaleSlider.Value;
+        }
+    }
+}
